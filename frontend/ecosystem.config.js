7fde3be468196028c826a91cde096e4b16db3e9b
Sergei Anokhin <sa.anokhin@yandex.ru>
2023-03-21 16:25:20 -0700

    Test 12

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       "post-deploy":
-        "cd current/frontend && npm i && npm run build",
+        "cd current && npm i && npm run build",
     },
   },
 };
