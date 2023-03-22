703f47ba9d2374f35a8292e2a2e41f6ecb6fff67
Sergei Anokhin <sa.anokhin@yandex.ru>
2023-03-21 21:34:25 -0700

    Исправления CORS 2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://api.anokhin-mesto.nomoredomains.work');
+  const api = new Api('https://api.anokhin-mesto.nomoredomains.work');
 
   export default api;
