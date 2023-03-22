03136117fdcb32f9d0c5dfdaface4cda40314692
Sergei Anokhin <sa.anokhin@yandex.ru>
2023-03-21 21:08:48 -0700

    Доработка CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,15 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://anokhin-mesto.nomoredomains.work',
+      'http://anokhin-mesto.nomoredomains.work',
+    ],
+    optionsSuccessStatus: 200,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
